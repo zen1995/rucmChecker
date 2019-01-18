cbdcd65dd798494a78cb4f2d905bcc27219f6467
杨森 <sen.yang96@outlook.com>
2019-01-18 21:52:50 +0800

    convert report to html and pdf

--- reporter.py
+++ reporter.py
@@ -1,5 +1,7 @@
 import sys
 import typing
+import jinja2
+import pdfkit 
 
 
 class ErrorInfo:
@@ -27,6 +29,32 @@ class Reporter():
             destination.write(
                 f"Sentence ({e.sentence}) in use case({e.usecasename}) violates the rule({e.rulename}).\n")
 
+    @staticmethod
+    def to_html(destination_file):
+        loader = jinja2.FileSystemLoader('./base.html')
+        env = jinja2.Environment(loader=loader)
+        template = env.get_template('')
+
+        items = []
+        for e in Reporter.errors:
+            items.append({
+                'description': e.rulename,
+                'usecasename': e.usecasename,
+                'sentence': e.sentence
+            })
+
+        result = template.render(items=items)
+
+        with open(destination_file, "w") as text_file:
+            print(result, file=text_file)
+
+        return result
+
+    @staticmethod
+    def to_pdf(destination_file):
+        Reporter.to_html('temp.html')
+        pdfkit.from_file('temp.html', destination_file) 
+
 
 if __name__ == "__main__":
     print('-' * 20, 'test Report', '-' * 20)
@@ -39,3 +67,5 @@ if __name__ == "__main__":
         Reporter.reporter.write(
             f"Sentence ({e.sentence}) in use case({e.usecasename}) violates the rule({e.rulename}).\n")
     print('-' * 60)
+    print(Reporter.to_html('sdf.html'))
+    Reporter.to_pdf('sdf.pdf')
